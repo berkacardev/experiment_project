3216f038ba4cc09a59542ba64310620553eed6e3
Berk Acar <berkacar.dev@gmail.com>
2024-03-21 11:14:54 +0300

    first projecet created

---
+++ android/app/src/main/kotlin/org/experimentproject/experiment_project/MainActivity.kt
@@ -0,0 +1,6 @@
+package org.experimentproject.experiment_project
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/feature/home/view/mixin/home_view_mixin.dart
@@ -0,0 +1,6 @@
+import 'package:experiment_project/feature/home/view/home_view.dart';
+import 'package:flutter/material.dart';
+
+mixin HomeViewMixin on State<HomeView>{
+
+}
